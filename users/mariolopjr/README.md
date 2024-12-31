6de871c467fe64ae095ee574c2bf7536f5d2ec47
Mario Lopez <mario@techmunchies.net>
2024-12-31 10:37:34 -0500

    migrate configuration from my qmk_firmware branch

---
+++ users/mariolopjr/README.md
@@ -0,0 +1,22 @@
+# keyboards
+ - Mode Designs Sonnet [75%] (2024)
+ - Mode Designs Envoy [65%]
+
+## qmk commands
+build clangd database: `qmk generate-compilation-database -kb mode/m75h -km mariolopjr`
+build sonnet: `qmk compile`
+full build sonnet: `qmk compile -kb mode/m75h -km mariolopjr`
+build envoy: `qmk compile -kb mode/m256wh`
+flash sonnet: `qmk flash`
+flash sonnet: `qmk flash -kb mode/m256wh`
+
+## update
+Quick instructions in updating from upstream
+```bash
+git checkout main
+git pull --rebase upstream main
+git pull --tags
+git submodule update --init --recursive
+git checkout keebs
+git merge main
+```
---
+++ users/mariolopjr/userspace.c
@@ -0,0 +1,40 @@
+/* Copyright 2023 mariolopjr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "userspace.h"
+
+/**
+ * @brief Main user keycode handler
+ *
+ * This handles all of the keycodes for the user, including calling feature handlers.
+ *
+ * @param keycode Keycode from matrix
+ * @param record keyrecord_t data structure
+ * @return true Continue processing keycode and send to host
+ * @return false Stop process keycode and do not send to host
+ */
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // Prints firmware info
+        case VRSN: // Prints firmware version
+            if (record->event.pressed) {
+                send_string_with_delay_P(PSTR(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION " | " QMK_BUILDDATE), MACRO_TIMER);
+            }
+            return false;
+
+        default:
+            return true;
+    }
+}
